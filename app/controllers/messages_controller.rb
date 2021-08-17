00e5b3c0f644883d073c468d9d5db37b711248d9
pukutan2021 <86871601+pukutan2021@users.noreply.github.com>
2021-08-17 15:30:15 +0900

    ビューとcssの修正

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,5 +1,10 @@
 class MessagesController < ApplicationController
+  before_action :set_message, only: [:edit, :update, :destroy, :show]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :destroy]
+  before_action :contributor_confirmation, only: [:edit, :update, :destroy]
+
   def index
+    @messages = Message.includes(:user).order('created_at DESC')
     @message = Message.new
   end
 
@@ -8,13 +13,39 @@ class MessagesController < ApplicationController
     if @message.save
       redirect_to root_path
     else
-      render :index
+      render :new
     end
+  end
+
+  def show
+  end
+
+  def update
+    if @message.update(message_params)
+      redirect_to message_path(@message)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @message.destroy
+    redirect_to root_path
   end
 
   private
 
   def message_params
     params.require(:message).permit(:content, :image).merge(user_id: current_user.id)
+  end
+
+  def set_message
+    @message = Message.find(params[:id])
+  end
+
+  def contributor_confirmation
+    if current_user != @message.user
+      redirect_to root_path
+    end
   end
 end
